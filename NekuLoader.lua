547e14c2e403cb6d14d2ca4e4dad1984bb474b38
coaled <108345407+coaled@users.noreply.github.com>
2022-07-10 14:43:12 +0200

    added power simulator 2

--- NekuLoader.lua
+++ NekuLoader.lua
@@ -172,3 +172,91 @@ local function DIGP_fake_script() -- loading.LocalScript
 end
 coroutine.wrap(DIGP_fake_script)()
 end
+-- Power Simulator 2
+if game.PlaceId == 4865990095 then
+	
+	
+local NekuLoader = Instance.new("ScreenGui")
+local main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local title = Instance.new("TextLabel")
+local loading = Instance.new("TextLabel")
+local ImageLabel = Instance.new("ImageLabel")
+local UICorner_2 = Instance.new("UICorner")
+
+--Properties:
+
+NekuLoader.Name = "NekuLoader"
+NekuLoader.Parent = game.CoreGui
+NekuLoader.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+main.Name = "main"
+main.Parent = NekuLoader
+main.BackgroundColor3 = Color3.fromRGB(57, 57, 57)
+main.Position = UDim2.new(0.354110211, 0, 0.413642943, 0)
+main.Size = UDim2.new(0, 322, 0, 119)
+main.Active = true
+main.Draggable = false
+
+UICorner.Parent = main
+
+title.Name = "title"
+title.Parent = main
+title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+title.BackgroundTransparency = 1.000
+title.Size = UDim2.new(0, 322, 0, 27)
+title.Font = Enum.Font.SourceSans
+title.Text = "Loading Neku Hub"
+title.TextColor3 = Color3.fromRGB(255, 170, 255)
+title.TextScaled = true
+title.TextSize = 14.000
+title.TextWrapped = true
+
+loading.Name = "loading"
+loading.Parent = main
+loading.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+loading.BackgroundTransparency = 1.000
+loading.Position = UDim2.new(0.33540374, 0, 0.285714298, 0)
+loading.Size = UDim2.new(0, 214, 0, 56)
+loading.Font = Enum.Font.SourceSans
+loading.Text = "Loading"
+loading.TextColor3 = Color3.fromRGB(85, 170, 255)
+loading.TextScaled = true
+loading.TextSize = 14.000
+loading.TextWrapped = true
+
+ImageLabel.Parent = main
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.0652173907, 0, 0.285714298, 0)
+ImageLabel.Size = UDim2.new(0, 87, 0, 74)
+ImageLabel.Image = "rbxassetid://10148474611"
+
+UICorner_2.Parent = ImageLabel
+
+-- Scripts:
+
+local function DIGP_fake_script() -- loading.LocalScript 
+	local script = Instance.new('LocalScript', loading)
+
+	local loading = script.Parent
+	
+	wait(0.5)
+	loading.Text = "Checking Game."
+	wait(0.5)
+	loading.Text = "Checking Game.."
+	wait(0.5)
+	loading.Text = "Checking Game..."
+	wait(0.5)
+	loading.Text = "Checking Game."
+	wait(0.5)
+	loading.Text = "Found Game!"
+	wait(0.5)
+	loading.Text = "Authentication Success"
+	wait(1)
+	loading.Text = "Enjoy!"
+	wait(1.5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/trotrosalaYT/scripts/main/Power%20Simulator%202.lua", true))()
+	loading.Parent:Destroy()
+end
+coroutine.wrap(DIGP_fake_script)()
